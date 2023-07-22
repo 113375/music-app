29a8bbf608f5dba3739b755e975049c41db8129d
113375 <knyazevdima05@gmail.com>
2023-07-22 14:52:55 +0300

    ready player

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import Player from "./components/Player";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Player />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Player/>
+        </div>
+    );
 }
